d327649d7464574adadd5dff990573608bfe8217
Justin Tran <justin.s.tran97@gmail.com>
2020-02-21 21:37:22 -0800

    Update NodeMCU_ESP8266_SerialComm_Test.ino

--- NodeMCU_ESP8266_SerialComm_Test/NodeMCU_ESP8266_SerialComm_Test.ino
+++ NodeMCU_ESP8266_SerialComm_Test/NodeMCU_ESP8266_SerialComm_Test.ino
@@ -1,5 +1,8 @@
 int data; //Initialized variable to store recieved data
-char rx_String[20];
+//char* rx_String[20];
+char rx_String[32];
+int numBytes;
+
 
 void setup() {
   // Serial communication begins at 9600 baud
@@ -20,23 +23,43 @@ void loop() {
   }
   */
 
+  while (Serial.available()) {
+    numBytes = Serial.available(); // Number of bytes available for reading
+                                   // from serial port
+    // Store each byte read into char array
+    for (int i = 0; i < numBytes; i++) {
+      delay(1);
+      rx_String[i] = Serial.read(); 
+    }
 
+    Serial.println("Before rx_String prints");
+    Serial.println(rx_String);
+    Serial.println("After rx_String prints");
+  }
+  
+/*
   if(Serial.available()){
-    rx_String = '\0'; // Reset cstring
+    rx_String[0] = '\0'; // Reset cstring
     String strArr[2]; //Set the size of the array to equal the number of values you will be receiveing.
+    char* rx_Ch; // Empty char
+
+    //strcpy(rx_String, rx_Ch);
     
     //Keep looping until there is something in the buffer.
     while (Serial.available()) {
       //Delay to allow byte to arrive in input buffer.
       delay(2);
+      
       //Read a single character from the buffer.
-      char ch = Serial.read();
+      rx_Ch = Serial.read();
+      
       //Append that single character to a string.
-      rx_String += ch;
+      //rx_String += rx_Ch;
+      strcat(rx_String, *rx_Ch);
 
       Serial.println(rx_String);
     }
-
+*/
 /*
   if(Serial.available()){
     String rxString = ""; // Empty string
