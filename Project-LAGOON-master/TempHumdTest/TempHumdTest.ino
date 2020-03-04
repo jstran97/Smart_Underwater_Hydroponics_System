18841e5bbe4cd25476320cb05bcecabd212a57c2
Justin Tran <justin.s.tran97@gmail.com>
2020-03-03 16:07:37 -0800

    Update TempHumdTest.ino

--- Project-LAGOON-master/TempHumdTest/TempHumdTest.ino
+++ Project-LAGOON-master/TempHumdTest/TempHumdTest.ino
@@ -1,6 +1,15 @@
 #include "Adafruit_HTU21DF.h"
 #include <Wire.h>
 
+int count;
+//char rand_str[2];
+char str[] = "yo";
+//String rand_str = "";
+//String new_str = "";
+//int temp_int = 123;
+float temp_float = 7.4;
+char test[30];
+
 //Create an instance of the object
 Adafruit_HTU21DF myHumidity;
 void setup()
@@ -26,18 +35,32 @@ void loop()
   //Serial.println("Time:");
   //Serial.println(millis());
 
+  Serial.println("Before sprintf()");
+
   Serial.println(" Humidity:");
   Serial.println(humd, 1);
   //Serial.println("%");
-  Serial1.write(humd);
-  delay(1000);
+  //Serial1.write(humd);
+  //delay(1000);
   
   Serial.println(" Temperature:");
   Serial.println(temp, 1);
   //Serial.println("C");
-  Serial1.write(temp);
+  //Serial1.write(temp);
   //delay(1000);
   
   
-  delay(10000);
+  // sprintf() with 3 ints (does NOT support float, double, and long long) WORKS
+  //sprintf(test, "%03d.%01d,%03d,%03d", (int)temp_float, (int)(temp_float*100)%100, 47, 96);
+  sprintf(test, "%03d.%01d,%03d,%03d", (int)temp_float, (int)(temp_float*100)%100, humd, temp);
+  Serial.println("After sprintf() & Before Serial.println(test)");
+  Serial.println(test);
+
+  Serial.println("Before Serial1.write(test)");
+  Serial1.write(test);
+  Serial.println("After Serial1.write(test)");
+
+
+  delay(1000);
+  //delay(10000);
 }
