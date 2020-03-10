518f1860cff759bc6d9a35f72b2aaeea2837e52e
Justin Tran <justin.s.tran97@gmail.com>
2020-03-10 15:44:49 -0700

    Update Firebase_NodeMCU_ESP8266.ino

--- Firebase_NodeMCU_ESP8266/Firebase_NodeMCU_ESP8266.ino
+++ Firebase_NodeMCU_ESP8266/Firebase_NodeMCU_ESP8266.ino
@@ -1,31 +1,20 @@
-//
-// Copyright 2015 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-
-// FirebaseDemo_ESP8266 is a sample that demo the different functions
-// of the FirebaseArduino API.
-
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
+#include <ctype.h>
 
-#define WIFI_SSID "iPhone69 Hotspot"
-#define WIFI_PASSWORD "PACEISCOOL"
-//#define WIFI_SSID "iPhone69 Hotspot"
-//#define WIFI_PASSWORD "PACEISCOOL"
+#define WIFI_SSID "will_hs"
+#define WIFI_PASSWORD "lagoon2020"
 #define FIREBASE_HOST "lagoon-9de60.firebaseio.com"
 #define FIREBASE_AUTH "aWjHUYVg9d7ZJEqKVW8tgaRHf4WlRt90FxFgze3H"
+
+int count = 1; // Indicates how many values were updated in database
+int serial_val_count = 1;
+int hum_val = 50;
+int pH_val = 3;
+int temperature = 20;
+int waterLevel_status = 0;
+
+int numBytes;
 
 void setup() {
   Serial.begin(9600);
@@ -44,48 +33,93 @@ void setup() {
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
-int count = 1; // Indicates how many values were updated in database
-int serial_val_count = 1;
-int hum_val = 50;
-int pH_val = 3;
-int temperature = 20;
-String waterLevel_status = "Red";
-
 void loop() {
-  if (Serial.available()) {
-    //Serial.write(Serial.read());
 
-    if (serial_val_count == 1) {
-      // Update humidity value
-      updateDBValues("/Update/Hum", Serial.read());
-      Serial.println("serial_val_count = 1 " + Serial.read());
-      serial_val_count++; // Increase count of received values from serial comm
-    }
-    //else if (serial_val_count == 2) {
-      // Update pH value
-      //updateDBValues("/Update/PH", Serial.read());
-      serial_val_count++; // Increase count of received values from serial comm
-    //}
-    else if (serial_val_count == 2) {
-      // Update temperature value
-      updateDBValues("/Update/Temp", Serial.read());
-      serial_val_count == 1; // Reset to 1
-      Serial.println("serial_val_count = 2 " + Serial.read());
-      
-      count++; // Increase number of values pushed to database
+  if (Serial.available()) {
+    numBytes = Serial.available(); // Number of bytes available for reading
+                                   // from serial port
+    char rx_String[numBytes];
+    // Store each byte read into char array
+    for (int i = 0; i < numBytes; i++) {
+      delay(1);
+      rx_String[i] = Serial.read(); 
     }
     
-  }
+    float rx_Data[4]; // New array to store strings converted to numbers
+    int float_index = 0;
+    //String temp;
+
+    char *ch_ptr = NULL;
+    char *temp_ch_arr[5];
+    byte index = 0;
   
-  delay(10000); // 10 sec delay
-  //delay(300000); // 5 min delay 
+    Serial.println("Before rx_String prints");
+    Serial.println(rx_String);
+
+
+    Serial.println("Before 1st ptr = strtok()");
+    ch_ptr = strtok(rx_String, ","); // Look for decimal point (.) delimiter
+    Serial.println(ch_ptr);
+
+    rx_Data[float_index] = atof(ch_ptr); // Convert alphabet to float value (including 
+                                   // decimal point)
+    float_index++;
+    Serial.print("Float #: ");
+    Serial.print(rx_Data[0]);
+    Serial.println(" ");
+
+
+     while (float_index < 3) {
+      delay(1);
+      ch_ptr = strtok(NULL, ","); // Look for next start of token with comma (,)
+      //temp_ch_arr[index] = ch_ptr; // Store char pointer to start of token found
+      rx_Data[float_index] = atof(ch_ptr); // Convert alphabet to float value (including 
+                                     // decimal point)
+      Serial.print("Point: ");
+      Serial.print(atof(ch_ptr) );
+      Serial.println(" ");
+      
+      //Serial.print("Float #: ");
+      //Serial.print(rx_Data[float_index]); 
+      //Serial.println(" "); 
+
+      float_index++;
+
+      //index++; 
+      
+    }
+
+
+
+    // Update humidity value
+    //updateDBValues("/Update/Hum", (int)rx_Data[1]);
+    updateDBValues("/Update/Hum", rx_Data[0]);
+  
+    // Update pH value
+    //updateDBValues("/Update/PH", (int)rx_Data[0]);
+    updateDBValues("/Update/PH", rx_Data[1]);
+    
+    // Update temperature value
+    //updateDBValues("/Update/Temp", (int)rx_Data[2]);
+    updateDBValues("/Update/Temp", rx_Data[2]);
+
+
+
+    count++; // Increase number of values pushed to database
+
+    delay(1000);
+  }
+
+  delay(1000);
+  //delay(10000); // 10 sec delay 
+  
 }
 
 
 // Update the date & time, humidity, pH, and temperature values in real-time database
-void updateDBValues(String db_path, int sensor_value) {
+void updateDBValues(String db_path, float sensor_value) {
   // set value in Update path of database, e.g. "/Update/Hum"
-  Firebase.setInt(db_path, sensor_value);
+  Firebase.setFloat(db_path, sensor_value);
   // handle error
   if (Firebase.failed()) {
       Serial.print("setting " + db_path + " failed:");
@@ -109,21 +143,8 @@ void updateDBValues(String db_path, int sensor_value) {
   
   newRecord_path.concat(count);  // Concatenates string with number var "count"
 
-/*
-  // append a new value to path of new value
-  String name = Firebase.pushInt(newRecord_path, count++); // 
-  // handle error
-  if (Firebase.failed()) {
-      Serial.print("pushing " + newRecord_path + " failed:");
-      Serial.println(Firebase.error());  
-      return;
-  }
-  Serial.print("pushed: " + newRecord_path);
-  Serial.println(name);
-  delay(100);
-*/
   // set new value for desired path in database, e.g. "/Humidity/Hum"
-  Firebase.setInt(newRecord_path, sensor_value);
+  Firebase.setFloat(newRecord_path, sensor_value);
   // handle error
   if (Firebase.failed()) {
       Serial.print("setting " + newRecord_path + " failed:");
