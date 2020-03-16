19970be2bee97e16f89a4463c59ce97d9c43efeb
Justin Tran <justin.s.tran97@gmail.com>
2020-03-16 13:49:03 -0700

    Update TempHumdTest.ino

--- Project-LAGOON-master/TempHumdTest/TempHumdTest.ino
+++ Project-LAGOON-master/TempHumdTest/TempHumdTest.ino
@@ -2,12 +2,7 @@
 #include <Wire.h>
 
 int count;
-//char rand_str[2];
-char str[] = "yo";
-//String rand_str = "";
-//String new_str = "";
-//int temp_int = 123;
-float temp_float = 7.4;
+float temp_float = 5.3;
 char test[30];
 
 //Create an instance of the object
@@ -31,6 +26,7 @@ void loop()
 {
   int humd = myHumidity.readHumidity();
   int temp = myHumidity.readTemperature();
+  int water_level = -1;
 
   //Serial.println("Time:");
   //Serial.println(millis());
@@ -51,8 +47,10 @@ void loop()
   
   
   // sprintf() with 3 ints (does NOT support float, double, and long long) WORKS
-  //sprintf(test, "%03d.%01d,%03d,%03d", (int)temp_float, (int)(temp_float*100)%100, 47, 96);
-  sprintf(test, "%03d.%01d,%03d,%03d", (int)temp_float, (int)(temp_float*100)%100, humd, temp);
+  //sprintf(test, "%03d.%01d,%03d,%03d", (int)temp_float, 
+  //    (int)(temp_float*100)%100, 47, 96);
+  sprintf(test, "%03d.%01d,%03d,%03d,%01d", (int)temp_float, 
+    (int)(temp_float*100)%100, humd, temp, water_level);
   Serial.println("After sprintf() & Before Serial.println(test)");
   Serial.println(test);
 
@@ -61,6 +59,6 @@ void loop()
   Serial.println("After Serial1.write(test)");
 
 
-  delay(1000);
-  //delay(10000);
+  //delay(1000);
+  delay(10000);
 }
