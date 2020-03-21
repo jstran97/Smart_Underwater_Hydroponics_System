afdc0b2f9b3a030b6da1fe2442312a41894a2c5e
Justin Tran <justin.s.tran97@gmail.com>
2020-03-21 11:21:26 -0700

    Add files via upload

---
+++ Firebase_NodeMCU_ESP8266_FetchData_Example/Firebase_NodeMCU_ESP8266_FetchData_Example.ino
@@ -0,0 +1,66 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+#include <ctype.h>
+
+
+#define WIFI_SSID "NETWORK_NAME_HERE"
+#define WIFI_PASSWORD "NETWORK_PASSWORD_HERE"
+#define FIREBASE_HOST "FIREBASEIO_LINK_HERE"
+#define FIREBASE_AUTH "AUTH_KEY_HERE"
+
+
+int count = 1; // Indicates how many values were updated in database
+int serial_val_count = 1;
+int hum_val = 50;
+int pH_val = 3;
+int temperature = 20;
+int waterLevel_status = 0;
+
+int numBytes;
+
+void setup() {
+  Serial.begin(9600);
+
+  // connect to Wi-Fi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("Connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+void loop() {
+
+  String path = "/Update/WL";
+  int val = Firebase.getInt(path);
+
+  Serial.print("Value: ");
+  Serial.print(val);
+  Serial.println(" ");
+
+/*
+  String path = "/";
+  FirebaseObject obj = Firebase.get(path); // Retrieve data from entire database
+
+  // Extract data with respect to different keys using which it is stored???? 
+  String obj_name = obj.getString("Name");
+  String obj_email = obj.getString("Email");
+  String obj_contact = obj.getString("Contact");
+  //String str_val = Firebase.getString(path);
+
+  
+  Serial.println("Strings: ");
+  //Serial.println(obj_name);
+  //Serial.println(obj_email);
+  //Serial.println(obj_contact);
+  Serial.println(str_val);
+  Serial.println(" ");
+*/
+  delay(10000);
+}
